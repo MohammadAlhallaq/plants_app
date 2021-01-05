67e3c93d02ff6f9806373fab2424476319c07df0
Mohammad <mohammad.alhallaq1@gmail.com>
2021-01-05 21:54:07 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'constraints.dart';
+import 'screens/home/components/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget{
+  // this is the root of the application
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Demo',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home:HomeScreen()
+    );
+  }
+}
